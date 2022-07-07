0c126a4a76d61da7a26b7475affa08d52484c34b
NipunGupta2k <aa.ngcsgo@gmail.com>
2022-07-07 15:14:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 1codefest
+ Hi, Ive created a simple elearning website single-page app, with bare basics and low time, with inspired design. Hope you like it, thanks for the codefest, and love the content on thapa yt channel! https://nipunsapp-thapacodefest.web.app/
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+/*Not used Footer currently, due to styling issues and less time! */
+
+/*
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-light text-center fixed-bottom" >
+                <p >
+                    © Nipun Gupta | Project submitted for Thapa Technical Codefest | July 2022
+                </p>
+            </footer>
+        </>
+    )
+}
+export default Footer;
+*/
